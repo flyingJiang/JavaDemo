7579f821c2b8defbbca935152aa062fe04465950
jiangjianfei <jiangjianfei@baidu.com>
2022-10-06 15:33:30 +0800

    811. 子域名访问计数

---
+++ src/test/java/com/example/demo/t20221001/T5subdomainVisits.java
@@ -0,0 +1,94 @@
+package com.example.demo.t20221001;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @program: demoes
+ * @description: 网站域名 "discuss.leetcode.com" 由多个子域名组成。顶级域名为 "com" ，二级域名为 "leetcode.com" ，最低一级为 "discuss.leetcode.com" 。当访问域名 "discuss.leetcode.com" 时，同时也会隐式访问其父域名 "leetcode.com" 以及 "com" 。
+ * <p>
+ * 计数配对域名 是遵循 "rep d1.d2.d3" 或 "rep d1.d2" 格式的一个域名表示，其中 rep 表示访问域名的次数，d1.d2.d3 为域名本身。
+ * <p>
+ * 例如，"9001 discuss.leetcode.com" 就是一个 计数配对域名 ，表示 discuss.leetcode.com 被访问了 9001 次。
+ * 给你一个 计数配对域名 组成的数组 cpdomains ，解析得到输入中每个子域名对应的 计数配对域名 ，并以数组形式返回。可以按 任意顺序 返回答案。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：cpdomains = ["9001 discuss.leetcode.com"]
+ * 输出：["9001 leetcode.com","9001 discuss.leetcode.com","9001 com"]
+ * 解释：例子中仅包含一个网站域名："discuss.leetcode.com"。
+ * 按照前文描述，子域名 "leetcode.com" 和 "com" 都会被访问，所以它们都被访问了 9001 次。
+ * 示例 2：
+ * <p>
+ * 输入：cpdomains = ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+ * 输出：["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+ * 解释：按照前文描述，会访问 "google.mail.com" 900 次，"yahoo.com" 50 次，"intel.mail.com" 1 次，"wiki.org" 5 次。
+ * 而对于父域名，会访问 "mail.com" 900 + 1 = 901 次，"com" 900 + 50 + 1 = 951 次，和 "org" 5 次。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= cpdomain.length <= 100
+ * 1 <= cpdomain[i].length <= 100
+ * cpdomain[i] 会遵循 "repi d1i.d2i.d3i" 或 "repi d1i.d2i" 格式
+ * repi 是范围 [1, 104] 内的一个整数
+ * d1i、d2i 和 d3i 由小写英文字母组成
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/subdomain-visit-count
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: jiangjianfei
+ * @create: 2022-10-06 15:21
+ **/
+public class T5subdomainVisits {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        List<String> ans = new ArrayList<String>();
+        Map<String, Integer> counts = new HashMap<String, Integer>();
+        for (String cpdomain : cpdomains) {
+            int space = cpdomain.indexOf(' ');
+            int count = Integer.parseInt(cpdomain.substring(0, space));
+            String domain = cpdomain.substring(space + 1);
+            // getOrDefault
+            counts.put(domain, counts.getOrDefault(domain, 0) + count);
+            for (int i = 0; i < domain.length(); i++) {
+                if (domain.charAt(i) == '.') {
+                    String subdomain = domain.substring(i + 1);
+                    // getOrDefault
+                    counts.put(subdomain, counts.getOrDefault(subdomain, 0) + count);
+                }
+            }
+        }
+        // 遍历方式1 通过entrySet实现遍历
+        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+            String subdomain = entry.getKey();
+            int count = entry.getValue();
+            ans.add(count + " " + subdomain);
+        }
+        // 遍历方式2 通过lambda表达式进行遍历
+        counts.forEach((k, v) -> {
+            ans.add(v + " " + k);
+        });
+        // 遍历方式3
+        for (Integer v : counts.values()) {
+
+        }
+        // 遍历方式4 通过key遍历
+        for (String k : counts.keySet()) {
+
+        }
+        // 遍历方式5 通过Iterator迭代器实现遍历
+        Iterator<Map.Entry<String, Integer>> entries = counts.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry<String, Integer> entry = entries.next();
+            ans.add(entry.getValue() + " " + entry.getKey());
+        }
+
+
+        return ans;
+    }
+}
