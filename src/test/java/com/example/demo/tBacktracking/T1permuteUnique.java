e399efcd214c564e095055d7c7112e7fd2429532
jiangjianfei <jiangjianfei@baidu.com>
2022-10-06 17:27:49 +0800

    47. 全排列 II 回溯

---
+++ src/test/java/com/example/demo/tBacktracking/T1permuteUnique.java
@@ -0,0 +1,80 @@
+package com.example.demo.tBacktracking;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @program: demoes
+ * @description:
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ *  [1,2,1],
+ *  [2,1,1]]
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/permutations-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: jiangjianfei
+ * @create: 2022-10-06 16:18
+ **/
+public class T1permuteUnique {
+    boolean[] vis;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> perm = new ArrayList<Integer>();
+        vis = new boolean[nums.length];
+        // 要剪枝排序很重要
+        Arrays.sort(nums);
+        backtrack(nums, ans, 0, perm);
+        return ans;
+    }
+
+    public void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<Integer>(perm));
+            return;
+        }
+        // 递归内有循环递归，复杂度自然低不了，即使剪枝
+        for (int i = 0; i < nums.length; ++i) {
+            // 剪枝
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            System.out.println("  递归之前 => " + perm);
+            backtrack(nums, ans, idx + 1, perm);
+            vis[i] = false;
+            perm.remove(idx);
+            System.out.println("递归之后 => " + perm);
+        }
+    }
+
+    @Test
+    public void test1(){
+        int[] nums = {1,1,2};
+        permuteUnique(nums);
+
+    }
+}
