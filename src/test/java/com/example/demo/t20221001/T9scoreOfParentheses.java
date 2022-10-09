a0661a3323fe0e3dad7f5bbd0742dbf0745a61c7
jiangjianfei <jiangjianfei@baidu.com>
2022-10-09 22:35:49 +0800

    856. 括号的分数

---
+++ src/test/java/com/example/demo/t20221001/T9scoreOfParentheses.java
@@ -0,0 +1,109 @@
+package com.example.demo.t20221001;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @program: demoes
+ * @description: 856. 括号的分数
+ * 给定一个平衡括号字符串 S，按下述规则计算该字符串的分数：
+ *
+ * () 得 1 分。
+ * AB 得 A + B 分，其中 A 和 B 是平衡括号字符串。
+ * (A) 得 2 * A 分，其中 A 是平衡括号字符串。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入： "()"
+ * 输出： 1
+ * 示例 2：
+ *
+ * 输入： "(())"
+ * 输出： 2
+ * 示例 3：
+ *
+ * 输入： "()()"
+ * 输出： 2
+ * 示例 4：
+ *
+ * 输入： "(()(()))"
+ * 输出： 6
+ *  
+ *
+ * 提示：
+ *
+ * S 是平衡括号字符串，且只含有 ( 和 ) 。
+ * 2 <= S.length <= 50
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/score-of-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: jiangjianfei
+ * @create: 2022-10-09 22:29
+ **/
+public class T9scoreOfParentheses {
+    /**
+     * 方法三：计算最终分数和
+     * 根据题意，ss 的分数与 11 分的 ()() 有关。对于每个 ()()，它的最终分数与它所处的深度有关，如果深度为 \textit{bal}bal，那么它的最终分数为 2^\textit{bal}2
+     * bal
+     *  。我们统计所有 ()() 的最终分数和即可。
+     *
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode.cn/problems/score-of-parentheses/solution/gua-hao-de-fen-shu-by-leetcode-solution-we6b/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param s
+     * @return
+     */
+    public int scoreOfParentheses3(String s) {
+        int bal = 0, n = s.length(), res = 0;
+        for (int i = 0; i < n; i++) {
+            bal += (s.charAt(i) == '(' ? 1 : -1);
+            if (s.charAt(i) == ')' && s.charAt(i - 1) == '(') {
+                res += 1 << bal;
+            }
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public int scoreOfParentheses2(String s) {
+        Deque<Integer> st = new ArrayDeque<Integer>();
+        st.push(0);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.push(0);
+            } else {
+                int v = st.pop();
+                int top = st.pop() + Math.max(2 * v, 1);
+                st.push(top);
+            }
+        }
+        return st.peek();
+    }
+
+    public int scoreOfParentheses(String s) {
+        if (s.length() == 2) {
+            return 1;
+        }
+        int bal = 0, n = s.length(), len = 0;
+        for (int i = 0; i < n; i++) {
+            bal += (s.charAt(i) == '(' ? 1 : -1);
+            if (bal == 0) {
+                len = i + 1;
+                break;
+            }
+        }
+        if (len == n) {
+            return 2 * scoreOfParentheses(s.substring(1, n - 1));
+        } else {
+            return scoreOfParentheses(s.substring(0, len)) + scoreOfParentheses(s.substring(len));
+        }
+    }
+}
