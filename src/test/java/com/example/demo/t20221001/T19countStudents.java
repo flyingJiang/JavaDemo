4989ac778cde1e14ee6acd60eec277eaf21e7e82
jiangjianfei <jiangjianfei@baidu.com>
2022-10-19 23:18:28 +0800

    1700. 无法吃午餐的学生数量

---
+++ src/test/java/com/example/demo/t20221001/T19countStudents.java
@@ -0,0 +1,100 @@
+package com.example.demo.t20221001;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @program: demoes
+ * @description: 学校的自助午餐提供圆形和方形的三明治，分别用数字 0 和 1 表示。所有学生站在一个队列里，每个学生要么喜欢圆形的要么喜欢方形的。
+ * 餐厅里三明治的数量与学生的数量相同。所有三明治都放在一个 栈 里，每一轮：
+ * <p>
+ * 如果队列最前面的学生 喜欢 栈顶的三明治，那么会 拿走它 并离开队列。
+ * 否则，这名学生会 放弃这个三明治 并回到队列的尾部。
+ * 这个过程会一直持续到队列里所有学生都不喜欢栈顶的三明治为止。
+ * <p>
+ * 给你两个整数数组 students 和 sandwiches ，其中 sandwiches[i] 是栈里面第 i​​​​​​ 个三明治的类型（i = 0 是栈的顶部）， students[j] 是初始队列里第 j​​​​​​ 名学生对三明治的喜好（j = 0 是队列的最开始位置）。请你返回无法吃午餐的学生数量。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：students = [1,1,0,0], sandwiches = [0,1,0,1]
+ * 输出：0
+ * 解释：
+ * - 最前面的学生放弃最顶上的三明治，并回到队列的末尾，学生队列变为 students = [1,0,0,1]。
+ * - 最前面的学生放弃最顶上的三明治，并回到队列的末尾，学生队列变为 students = [0,0,1,1]。
+ * - 最前面的学生拿走最顶上的三明治，剩余学生队列为 students = [0,1,1]，三明治栈为 sandwiches = [1,0,1]。
+ * - 最前面的学生放弃最顶上的三明治，并回到队列的末尾，学生队列变为 students = [1,1,0]。
+ * - 最前面的学生拿走最顶上的三明治，剩余学生队列为 students = [1,0]，三明治栈为 sandwiches = [0,1]。
+ * - 最前面的学生放弃最顶上的三明治，并回到队列的末尾，学生队列变为 students = [0,1]。
+ * - 最前面的学生拿走最顶上的三明治，剩余学生队列为 students = [1]，三明治栈为 sandwiches = [1]。
+ * - 最前面的学生拿走最顶上的三明治，剩余学生队列为 students = []，三明治栈为 sandwiches = []。
+ * 所以所有学生都有三明治吃。
+ * 示例 2：
+ * <p>
+ * 输入：students = [1,1,1,0,0,1], sandwiches = [1,0,0,0,1,1]
+ * 输出：3
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= students.length, sandwiches.length <= 100
+ * students.length == sandwiches.length
+ * sandwiches[i] 要么是 0 ，要么是 1 。
+ * students[i] 要么是 0 ，要么是 1 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/number-of-students-unable-to-eat-lunch
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: jiangjianfei
+ * @create: 2022-10-19 23:01
+ **/
+public class T19countStudents {
+
+    public int countStudents(int[] students, int[] sandwiches) {
+        Integer one = 1;
+        Integer zero = 0;
+        // 队列
+        Queue<Integer> student = new LinkedList<>();
+        Queue<Integer> sandwichStack = new ArrayDeque<>();
+        for (int i = 0; i < students.length; i++) {
+            student.offer(students[i]);
+            sandwichStack.offer(sandwiches[i]);
+        }
+        while (!sandwichStack.isEmpty()) {
+            if (sandwichStack.peek().equals(one) && !student.contains(one)) {
+                return student.size();
+            }
+
+            if (sandwichStack.peek().equals(zero) && !student.contains(zero)) {
+                return student.size();
+            }
+            if (sandwichStack.peek().equals(student.peek())) {
+                sandwichStack.poll();
+                student.poll();
+                continue;
+            }
+            Integer toTail = student.poll();
+            student.offer(toTail);
+        }
+        return 0;
+    }
+
+    public int countStudents2(int[] students, int[] sandwiches) {
+        int s1 = Arrays.stream(students).sum();
+        int s0 = students.length - s1;
+        for (int i = 0; i < sandwiches.length; i++) {
+            if (sandwiches[i] == 0 && s0 > 0) {
+                s0--;
+            } else if (sandwiches[i] == 1 && s1 > 0) {
+                s1--;
+            } else {
+                break;
+            }
+        }
+        return s0 + s1;
+    }
+}
