617751107e4a4b9aafe9b26b89a2b5c34cef56db
jiangjianfei <jiangjianfei@baidu.com>
2022-10-29 13:22:25 +0800

    862. 和至少为 K 的最短子数组

---
+++ src/test/java/com/example/demo/t20221001/T25shortestSubarray.java
@@ -0,0 +1,63 @@
+package com.example.demo.t20221001;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @program: demoes
+ * @description: 862. 和至少为 K 的最短子数组
+ * 给你一个整数数组 nums 和一个整数 k ，找出 nums 中和至少为 k 的 最短非空子数组 ，并返回该子数组的长度。如果不存在这样的 子数组 ，返回 -1 。
+ *
+ * 子数组 是数组中 连续 的一部分。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1], k = 1
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：nums = [1,2], k = 4
+ * 输出：-1
+ * 示例 3：
+ *
+ * 输入：nums = [2,-1,2], k = 3
+ * 输出：3
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 105
+ * -105 <= nums[i] <= 105
+ * 1 <= k <= 109
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/shortest-subarray-with-sum-at-least-k
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author: jiangjianfei
+ * @create: 2022-10-29 13:21
+ **/
+public class T25shortestSubarray {
+    public int shortestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        long[] preSumArr = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            preSumArr[i + 1] = preSumArr[i] + nums[i];
+        }
+        int res = n + 1;
+        Deque<Integer> queue = new ArrayDeque<Integer>();
+        for (int i = 0; i <= n; i++) {
+            long curSum = preSumArr[i];
+            while (!queue.isEmpty() && curSum - preSumArr[queue.peekFirst()] >= k) {
+                res = Math.min(res, i - queue.pollFirst());
+            }
+            while (!queue.isEmpty() && preSumArr[queue.peekLast()] >= curSum) {
+                queue.pollLast();
+            }
+            queue.offerLast(i);
+        }
+        return res < n + 1 ? res : -1;
+    }
+}
