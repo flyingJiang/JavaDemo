67938168b6abe2a43d28ef7b09c9d5b139135bfd
jiangjianfei <jiangjianfei@baidu.com>
2022-11-03 23:01:07 +0800

    最大重复子字符串

---
+++ src/test/java/t202211/T03maxRepeating.java
@@ -0,0 +1,100 @@
+package t202211;
+
+import java.util.Arrays;
+
+/**
+ * @program: demoes
+ * @description: 1668. 最大重复子字符串
+ *
+ * 给你一个字符串 sequence ，如果字符串 word 连续重复 k 次形成的字符串是 sequence 的一个子字符串，那么单词 word 的 重复值为 k 。单词 word 的 最大重复值 是单词 word 在 sequence 中最大的重复值。如果 word 不是 sequence 的子串，那么重复值 k 为 0 。
+ *
+ * 给你一个字符串 sequence 和 word ，请你返回 最大重复值 k 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：sequence = "ababc", word = "ab"
+ * 输出：2
+ * 解释："abab" 是 "ababc" 的子字符串。
+ * 示例 2：
+ *
+ * 输入：sequence = "ababc", word = "ba"
+ * 输出：1
+ * 解释："ba" 是 "ababc" 的子字符串，但 "baba" 不是 "ababc" 的子字符串。
+ * 示例 3：
+ *
+ * 输入：sequence = "ababc", word = "ac"
+ * 输出：0
+ * 解释："ac" 不是 "ababc" 的子字符串。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= sequence.length <= 100
+ * 1 <= word.length <= 100
+ * sequence 和 word 都只包含小写英文字母。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/maximum-repeating-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author: jiangjianfei
+ * @create: 2022-11-03 22:26
+ **/
+public class T03maxRepeating {
+
+    public int maxRepeating(String sequence, String word) {
+        String s = word;
+        int num = 0;
+        while (true) {
+            if (sequence.contains(s)) {
+                s += word;
+                num++;
+                continue;
+            } else {
+                return num;
+            }
+        }
+
+    }
+
+
+    // KMP 算法 + 动态规划
+    public int maxRepeating2(String sequence, String word) {
+        int n = sequence.length(), m = word.length();
+        if (n < m) {
+            return 0;
+        }
+
+        int[] fail = new int[m];
+        Arrays.fill(fail, -1);
+        for (int i = 1; i < m; ++i) {
+            int j = fail[i - 1];
+            while (j != -1 && word.charAt(j + 1) != word.charAt(i)) {
+                j = fail[j];
+            }
+            if (word.charAt(j + 1) == word.charAt(i)) {
+                fail[i] = j + 1;
+            }
+        }
+
+        int[] f = new int[n];
+        int j = -1;
+        for (int i = 0; i < n; ++i) {
+            while (j != -1 && word.charAt(j + 1) != sequence.charAt(i)) {
+                j = fail[j];
+            }
+            if (word.charAt(j + 1) == sequence.charAt(i)) {
+                ++j;
+                if (j == m - 1) {
+                    f[i] = (i >= m ? f[i - m] : 0) + 1;
+                    j = fail[j];
+                }
+            }
+        }
+
+        return Arrays.stream(f).max().getAsInt();
+    }
+}
+
