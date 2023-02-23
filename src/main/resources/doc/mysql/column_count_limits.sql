ab0d5c0f074fc8369ca68a0f89edd1531c593cc6
jiangjianfei <jiangjianfei@baidu.com>
2023-02-23 21:35:14 +0800

    sql 关于需求过程中是新增表，还是已有表的基础上扩充字段的调研

---
+++ src/main/resources/doc/mysql/column_count_limits.sql
@@ -0,0 +1,11 @@
+-- MySQL表字段数量限制及行大小限制详情
+-- http://www.45fan.com/article.php?aid=1D5EWoe9cU5hySOX
+
+-- mysql 一个表中存多少个字段儿合理_20个MySQL高性能架构设计原则（收藏版）
+-- https://blog.csdn.net/weixin_39697660/article/details/111694034
+--
+-- 为什么大家说 MySQL 数据库单表最大两千万？依据是啥？
+-- https://blog.csdn.net/WantFlyDaCheng/article/details/124564511
+
+-- MySQL到底能有多少个字段
+-- https://blog.51cto.com/u_11583583/4877368
